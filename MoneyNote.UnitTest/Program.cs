5274e6dd258cc5a7e09cd7b88abe6aed22520449
nguyen phan du <badpaybad@gmail.com>
2020-06-19 00:43:43 +0700

    try do test thread save for concurrent dictionary

---
+++ MoneyNote.UnitTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MoneyNote.UnitTest
+{
+    class Program
+    {
+        static ConcurrentDictionary<string, string> _cache = new ConcurrentDictionary<string, string>();
+        static List<string> result = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Parallel.For(0, 10, (i) =>
+            {
+                string value = $"begin_{i}_{DateTime.Now.Ticks}";
+                Console.WriteLine(value);
+                var x = _cache.GetOrAdd("test", BuildCache(i));
+                result.Add($"{i}_Result_{x}");
+            });
+
+            foreach(var r in result)
+            {
+                Console.WriteLine(r);
+            }
+            Console.ReadLine();
+        }
+
+        public static string BuildCache(int i)
+        {
+            string value = $"{i}_inner_{DateTime.Now.Ticks}";
+            Console.WriteLine(value);
+
+            Thread.Sleep(2000);
+            return $"{i}_Data_{value    }";
+        }
+    }
+}
